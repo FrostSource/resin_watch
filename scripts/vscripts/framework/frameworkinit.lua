fad52ed834e273f5c1191f2ef7513da2292c12df
FrostSource <jjhfrost@gmail.com>
2024-01-31 10:17:04 +1300

    Update frameworkinit

--- scripts/vscripts/framework/frameworkinit.lua
+++ scripts/vscripts/framework/frameworkinit.lua
@@ -1,18 +1,27 @@
--- Don't use frameworkinit if Scalable Init Support is enabled
-if not IsInToolsMode() and Convars:GetStr("default_enabled_addons_list"):find("2182586257") then
-    return
-end
+--[[
+    v2.0.0
+    https://github.com/FrostSource/hla_extravaganza
+
+    Used to automatically load `core` and any addon scripts.
+
+    Checks if Scalable init Support is enabled before requiring specific addon scripts.
+]]
 
 if IsServer() then
     local success, result = pcall(require, "core")
     if not success then
-        Warning("core.lua failed to initialized\n")
-        Warning(tostring(result).."\n")
-        if IsInToolsMode() then
-            print("core.lua failed to initialized")
-            print(tostring(result))
-        end
+        warn("core.lua failed to initialized\n")
+        warn(tostring(result).."\n")
     end
 
-    require "resin_watch.init"
+    --
+    -- If Scalable Init Support is enabled, don't require these addon scripts
+    -- Instead the `mods/init/` folder should be supported.
+    --
+    -- If game is running in tools mode we will require even if Scalable Init Support is enabled
+    -- because it does not work even when displayed as enabled
+    --
+    if not Convars:GetStr("default_enabled_addons_list"):find("2182586257") or IsInToolsMode() then
+        require "resin_watch.init"
+    end
 end
